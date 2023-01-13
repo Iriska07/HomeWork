60a957314afd8dbd28d21c5afecd1b5791ede9d9
irisha.ved <irisha.ved@mail.ru>
2023-01-14 00:21:32 +0400

    Lesson3.1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите пятизначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [5];
+array[0] = num/10000;
+array[1] = (num/1000)%10;
+array[3] = (num/10)%10;
+array[4] = num%10;
+if (array[0] == array[4] && array[1] == array[3])
+{
+    Console.WriteLine("Палиндром");
+}
+else
+{
+    Console.WriteLine("Не палиндром");
+}
